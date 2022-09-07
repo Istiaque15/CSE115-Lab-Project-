d1d201e5cd4e599c4ce737432bb7100cfc4004a9
Istiaque KHan <107535180+Istiaque15@users.noreply.github.com>
2022-09-07 14:45:09 +0600

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,18 @@
+void meun_h(void){
+    printf("\t\t\t||=========================================================||\n");
+    printf("\t\t\t||<<<<<<<<<<<Welcome to Library management system>>>>>>>>>>||\n");
+    printf("\t\t\t||=========================================================||\n");
+
+    printf("\t\t\t \n\n<Features Of Our Library Management System>\n\n ");
+    printf("\t\t\tNo.1> User can add any book from Library\n");
+    printf("\t\t\tNo.2> User can see the added books list\n");
+    printf("\t\t\tNo.3> User can remove any added book from the list\n");
+    printf("\t\t\tNo.4> User can issue book from the list\n");
+    printf("\t\t\tNo.5> User can see issued books list\n\n");
+    printf("\t\t\t******Thank You For using Library Management System(LMS)******\n");
+    printf("\t\t\t________________________________________________________________ ");
+    sleep(10);
+    system("cls");
+
+
+}
